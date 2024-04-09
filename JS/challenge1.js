17fcb62bdcd16bc7ef86ba2fc4bc10a8af93bb96
LeoLeone43 <156853814+LeoLeone43@users.noreply.github.com>
2024-04-09 16:49:55 -0400

    Add files via upload

---
+++ JS/challenge1.js
@@ -0,0 +1,13 @@
+var create;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+});
+
+
+document.querySelector("h1").addEventListener("click",function(){
+	create.classList.add("heading");
+	console.log("change coloring");
+});
+
+// i think im on the right track i just needed more time to do this because im not good at it
